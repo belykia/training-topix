438638450bf808908b1c825fcfdaca3c6fce35db
belykia <belykia@outlook.com>
2018-04-24 06:26:38 -0400

    i'm working on master

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-print "hello World!"
+print "hello andrea!"
 
-for i in range(0,5):
+for i in range(0,4):
   print i
 
-for i in range(0,5):
+for i in range(0,4):
   print i
+
+  my_dict={}
+  my_dict['topix'] = 4
